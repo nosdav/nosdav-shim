df06215ff72f69d9f6635e90f8ba5cbae229f78c
Melvin Carvalho <melvincarvalho@gmail.com>
2023-10-07 19:51:45 +0200

    add u and METHOD

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const originalFetch = window.fetch;
+const originalFetch = window.fetch
 
-window.fetch = async function(url, options) {
-  const newOptions = { ...options };
-
+window.fetch = async function (url, options) {
+  const newOptions = { ...options }
 
   if (newOptions.method === 'PUT') {
     const event = {
       kind: 27235,
       created_at: Math.floor(Date.now() / 1000),
-      tags: [['url', url]],
+      tags: [
+        ['u', url],
+        ['method', 'PUT']
+      ],
       content: ''
     }
     const signedEvent = await window.nostr.signEvent(event)
-    var auth = `Nostr ${btoa(JSON.stringify(signedEvent))}`
-  
+    const auth = `Nostr ${btoa(JSON.stringify(signedEvent))}`
 
     newOptions.headers = {
       ...newOptions.headers,
-      'authorization': auth
-    };
+      authorization: auth
+    }
   }
 
-  return originalFetch.call(this, url, newOptions);
-};
+  return originalFetch.call(this, url, newOptions)
+}
